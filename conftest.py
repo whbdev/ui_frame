98023ffd1a8dd46b61459f05a794838ef2038d4e
whb <18715001424@163.com>
2022-09-02 13:54:06 +0800

    1.在conftest.py的user_driver方法中完善登录后保存cookie,在几分钟内访问项目可以实现免登录的功能

--- conftest.py
+++ conftest.py
@@ -2,10 +2,15 @@
 # @Author : WangHongBing
 # @File : conftest.py
 # @CreateTime : 2022/8/26 10:03:41
+import json
+import logging
+from pathlib import Path
 import pytest
 from webdriver_helper import get_webdriver
-
 from core import pom
+
+
+logger = logging.getLogger(__name__)
 
 
 @pytest.fixture(scope='class')
@@ -18,16 +23,35 @@ def driver():
 @pytest.fixture(scope='session')
 def user_driver():
     """前置登录"""
+    cookies = []
     driver = get_webdriver()
     driver.get("http://101.34.221.219:8010/")
 
-    page = pom.HomePage(driver)
-    page = page.to_login()
+    path = Path('temp/cookie/cookies.json')
+    if path.exists():
+        cookies = json.loads(path.read_text())
+    logger.info("加载cookie")
+    for cookie in cookies:
+        driver.add_cookie(cookie)
+        logger.warning(f"设置cookie:{cookie}")
 
-    page.login('whb01', 'admin123')
-    msg = page.get_msg()
+    driver.get("http://101.34.221.219:8010/")
 
-    assert '登录成功' == msg
+    if "退出" in driver.page_source:
+        pass
+    else:
+        page = pom.HomePage(driver)
+        page = page.to_login()
+
+        page.login('whb01', 'admin123')
+        msg = page.get_msg()
+
+        assert '登录成功' == msg
+
+        # 保存Cookie
+        cookie = driver.get_cookies()
+        with open('temp/cookie/cookies.json', 'w') as f:
+            f.write(json.dumps(cookie))
 
     yield driver
 
